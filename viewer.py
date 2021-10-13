e8ab0f8070fffd61f10bdacd3b46de41809350b1
Brian Keating <bkeating@healthlytix.com>
2021-10-12 19:20:47 -0700

    show 4 images rather than 3 views

--- viewer.py
+++ viewer.py
@@ -14,6 +14,8 @@ from hlxpy.volume import transpose, flip, vol_resample
 COLORMAP = px.colors.qualitative.D3  # https://plotly.com/python/discrete-color/
 SEGMENTATION_LABEL = 'Brain Tumor Overlay'
 
+
+
 def load_volume_for_display(uri):
     vol = io.volread(uri)
     vol = flip(transpose(vol, (2,1,0)), 0)
@@ -26,23 +28,21 @@ def load_seg_for_display(uri, ref_vol):
     return seg
 
 
+
 # load data
 case_name = 'C3L-00016'
-vol_dict = OrderedDict()
-vol_dict['T1'] = load_volume_for_display('data/C3L-00016/t1.nii.gz')
-vol_dict['T1ce'] = load_volume_for_display('data/C3L-00016/t1ce.nii.gz')
-# vol_dict['T2'] = load_volume_for_display('data/C3L-00016/t2.nii.gz')
-# vol_dict['FLAIR'] = load_volume_for_display('data/C3L-00016/flair.nii.gz')
+T1_vol = load_volume_for_display('data/C3L-00016/t1.nii.gz')
+T1ce_vol = load_volume_for_display('data/C3L-00016/t1ce.nii.gz')
+T2_vol = load_volume_for_display('data/C3L-00016/t2.nii.gz')
+FLAIR_vol = load_volume_for_display('data/C3L-00016/flair.nii.gz')
 
-ref_vol_name = list(vol_dict.keys())[0]
-ref_vol = vol_dict[ref_vol_name]
+ref_vol = T1_vol
 seg_dict = OrderedDict()
 seg_dict['None'] = np.zeros_like(ref_vol, dtype=np.uint8)
 seg_dict['Consensus'] = load_seg_for_display('data/C3L-00016/tumor-seg-consensus.nii.gz', ref_vol)
 # seg_dict['Seibert'] = load_seg_for_display('data/C3L-00016/tumor-seg-seibert.nii.gz', ref_vol)
 # seg_dict['Rudie'] = load_seg_for_display('data/C3L-00016/tumor-seg-rudie.nii.gz', ref_vol)
 # seg_dict['Ziseen'] = load_seg_for_display('data/C3L-00016/tumor-seg-ziseen.nii.gz', ref_vol)
-
 
 
 
@@ -59,66 +59,53 @@ seg_checkboxes = dcc.RadioItems(
 )
 seg_selection_div = html.Div([SEGMENTATION_LABEL+': ', seg_checkboxes], style={'display': 'inline'})
 
+
+
 # slice views
-axial_view = VolumeSlicer(app, np.asarray(ref_vol), spacing=ref_vol.voxel_size, axis=0)
-coronal_view = VolumeSlicer(app, np.asarray(ref_vol), spacing=ref_vol.voxel_size, axis=1)
-sagittal_view = VolumeSlicer(app, np.asarray(ref_vol), spacing=ref_vol.voxel_size, axis=2)
+T1_view = VolumeSlicer(app, np.asarray(T1_vol), spacing=T1_vol.voxel_size, axis=0)
+T1ce_view = VolumeSlicer(app, np.asarray(T1ce_vol), spacing=T1ce_vol.voxel_size, axis=0)
+T2_view = VolumeSlicer(app, np.asarray(T2_vol), spacing=T2_vol.voxel_size, axis=0)
+FLAIR_view = VolumeSlicer(app, np.asarray(FLAIR_vol), spacing=FLAIR_vol.voxel_size, axis=0)
 slice_views = html.Div(
     style={
         'display': 'grid',
-        'gridTemplateColumns': '33% 33% 33%'
+        'gridTemplateColumns': '50% 50%'
     },
     children=[
-        html.Div([axial_view.graph, html.Br(), axial_view.slider, *axial_view.stores], style={'margin': '0 10px'}),
-        html.Div([sagittal_view.graph, html.Br(), sagittal_view.slider, *sagittal_view.stores], style={'margin': '0 10px'}),
-        html.Div([coronal_view.graph, html.Br(), coronal_view.slider, *coronal_view.stores], style={'margin': '0 10px'})
+        html.Div([
+            html.Div([html.H3('T1', style={'text-align': 'center'}), T1_view.graph, html.Br(), T1_view.slider, *T1_view.stores], style={'margin': '20px'}),
+            html.Div([html.H3('T1ce', style={'text-align': 'center'}), T1ce_view.graph, html.Br(), T1ce_view.slider, *T1ce_view.stores], style={'margin': '20px'})
+        ]),
+        html.Div([
+            html.Div([html.H3('T2', style={'text-align': 'center'}), T2_view.graph, html.Br(), T2_view.slider, *T2_view.stores], style={'margin': '20px'}),
+            html.Div([html.H3('FLAIR', style={'text-align': 'center'}), FLAIR_view.graph, html.Br(), FLAIR_view.slider, *FLAIR_view.stores], style={'margin': '20px'})
+        ])
     ]
 )
-
-# volume selection
-vol_checkboxes = dcc.RadioItems(
-    id='vol-checkboxes',
-    options=[{'label': vol_name, 'value': vol_name} for vol_name in vol_dict.keys()],
-    value=ref_vol_name,
-    style={'display': 'inline'}
-)
-vol_selection_div = html.Div(['Volume: ', vol_checkboxes], style={'display': 'inline'})
 
 # main layout
 app.layout = html.Div(
     children=[
         html.H1(case_name),
-        slice_views,
+        seg_selection_div,
         html.Br(),
-        vol_selection_div,
-        html.Br(),
-        html.Br(),
-        seg_selection_div
+        slice_views
     ]
 )
 
 
 # callbacks
 @app.callback(
-    Output(axial_view.overlay_data.id, 'data'),
-    Output(sagittal_view.overlay_data.id, 'data'),
-    Output(coronal_view.overlay_data.id, 'data'),
+    Output(T1_view.overlay_data.id, 'data'),
+    Output(T1ce_view.overlay_data.id, 'data'),
+    Output(T2_view.overlay_data.id, 'data'),
+    Output(FLAIR_view.overlay_data.id, 'data'),
     Input('seg-checkboxes', 'value')
 )
 def change_overlay(selected_seg_name):
     seg = np.asarray(seg_dict[selected_seg_name])
-    return axial_view.create_overlay_data(seg, COLORMAP), sagittal_view.create_overlay_data(seg, COLORMAP), coronal_view.create_overlay_data(seg, COLORMAP)
+    return T1_view.create_overlay_data(seg, COLORMAP), T1ce_view.create_overlay_data(seg, COLORMAP), T2_view.create_overlay_data(seg, COLORMAP), FLAIR_view.create_overlay_data(seg, COLORMAP)
 
-
-# @app.callback(
-#     Output(axial_view.graph.id, 'data'),
-#     Output(sagittal_view.graph.id, 'data'),
-#     Output(coronal_view.graph.id, 'data'),
-#     Input('vol-checkboxes', 'value')
-# )
-# def change_volume(selected_vol_name):
-#     vol = np.asarray(vol_dict[selected_vol_name])
-#     return vol, vol, vol
 
 
 if __name__ == '__main__':
